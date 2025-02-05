0d3de7f6c46181579ee9218b115b1ea1feda85ac
Meruyert <22B31991@qyzpu.edu.kz>
2025-02-05 11:49:48 +0600

    Delete myproject directory

--- myproject/myproject/urls.py
+++
@@ -1,9 +0,0 @@
-# myproject/urls.py
-from django.contrib import admin
-from django.urls import path
-from . import views  # views файлы импортталды
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.home, name='home'),  # home көрінісі қосылды
-]
